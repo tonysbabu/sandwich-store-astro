483086b21f29520e8d63f535c7c62d91ee896549
Tony <tonysbabu@gmail.com>
2023-11-04 13:56:06 +0530

    added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import solidJs from '@astrojs/solid-js';
 import react from '@astrojs/react';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-frontend-masters.netlify.app',
-	output: 'hybrid',
-	experimental: {
-		hybridOutput: true,
-	},
-	integrations: [solidJs(), react()],
+  site: 'https://astro-frontend-masters.netlify.app',
+  output: 'hybrid',
+  experimental: {
+    hybridOutput: true
+  },
+  integrations: [solidJs(), react()],
+  adapter: netlify()
 });
