1390946a9c322678924300871327f4ce25a919fc
Tony <tonysbabu@gmail.com>
2023-11-04 17:24:42 +0530

    version fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import react from '@astrojs/react';
-
 import netlify from '@astrojs/netlify/functions';
+
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-frontend-masters.netlify.app',
 	output: 'server',
+	site: 'https://astro-frontend-masters.netlify.app',
 	experimental: {
 		assets: true,
 	},
-	integrations: [solidJs(), react()],
+	integrations: [solidJs(), react(), mdx()],
 	adapter: netlify(),
 });
